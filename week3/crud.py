06a24d559be1924720367dea91fd2273f1d65bc0
withonewith <sky30a@korea.ac.kr>
2024-04-04 13:14:51 +0900

    fix: 오타 수정

--- week3/crud.py
+++ week3/crud.py
@@ -7,7 +7,7 @@ def get_user(db: Session, user_id = int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
 # Read a single user by email
-def get_user_by_email(db: Session, email:Str)
+def get_user_by_email(db: Session, email:str):
     return db.query(models.User).filter(models.User.email == email).first()
 
 # Read multiple users
