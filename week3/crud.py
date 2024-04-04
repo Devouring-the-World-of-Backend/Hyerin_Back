425f354fc537e7c137948f5c058787b0f5fb3429
withonewith <sky30a@korea.ac.kr>
2024-04-04 13:13:09 +0900

    feat: Create 구현

--- week3/crud.py
+++ week3/crud.py
@@ -17,3 +17,18 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 # Read multiple items
 def get_books(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Book).offset(skip).limit(limit).all()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.password + "notreallyhashed"
+    db_user = models.User(email = user.email, hashed_password = fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def create_user_book(db: Session, item:schemas.BookCreate, user_id: int):
+    db_book = models.Book(**item.dict(), owner_id = user_id)
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
